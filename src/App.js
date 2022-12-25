e8f067436378cbcbe3b74522a697a3ab5f64e23f
baluonline <18106894+baluonline@users.noreply.github.com>
2022-12-25 12:27:01 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Hello world</h1>
+       <p>welcome to bicon, This page is under construction</p>
       </header>
     </div>
   );
