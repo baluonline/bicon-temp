d2ed2d1095810286a68a64bdb28223f11afdc7cf
baluonline <18106894+baluonline@users.noreply.github.com>
2023-02-19 20:56:11 -0600

    Developed contact us and footer

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -1,4 +1,4 @@
-import { Outlet, NavLink, Link } from "react-router-dom";
+import { Outlet, NavLink, Link, useNavigate   } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,11 +7,16 @@ import logo from "..//images/logo.png";
 import "../styles/footer.scss";
 import Footer from "./Footer";
 
+
 export default function RootLayout() {
+  const navigate = useNavigate();
+  const clickContactUs = () => {
+    navigate("/contact")
+  }
   return (
     <div className="root-layout">
-      <header>
-        <Navbar bg="light" expand="lg">
+      <header className="col-12 d-flex">
+        <Navbar className="col-12 " bg="light" expand="lg">
           <Container>
             <img href="/" className="App-logo" src={logo} />
             <Navbar.Brand as={Link} to="/">
@@ -32,15 +37,26 @@ export default function RootLayout() {
                 <Nav.Link as={Link} to="/about">
                   About Us
                 </Nav.Link>
-                <Nav.Link as={Link} to="/contact">
+                {/* <Nav.Link as={Link} to="/contact">
                   Contact Us
-                </Nav.Link>
+                </Nav.Link> */}
               </Nav>
             </Navbar.Collapse>
           </Container>
         </Navbar>
+        <button
+          type="button"
+          onClick={clickContactUs}
+          bg="none"
+          className="btn btn-primary contact-us-header col-2 primary"
+        >
+          Contact us
+          {/* <Nav.Link  as={Link} to="/contact">
+            Contact Us
+          </Nav.Link> */}
+        </button>
       </header>
-      <main  className="root-container">
+      <main className="root-container">
         <Outlet />
       </main>
       <footer id="footer">
