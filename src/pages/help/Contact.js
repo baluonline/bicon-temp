03c16e2159631f12e028cc15ff4b5be057abf51f
baluonline <18106894+baluonline@users.noreply.github.com>
2023-03-05 07:54:59 -0600

    Updated contact form

--- src/pages/help/Contact.js
+++ src/pages/help/Contact.js
@@ -1,9 +1,45 @@
+import { useState } from "react";
+import validator from "validator";
+
 import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 
 import "./../..//App.css";
 
 export default function Contact() {
+  const [fullName, setfullName] = useState("");
+  const [phoneNum, setPhoneNum] = useState("");
+  const [email, setEmail] = useState("");
+  const [yourMessage, setYourMessage] = useState("");
+  const [emailError, setEmailError] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    console.log(fullName);
+    console.log(phoneNum);
+    console.log(email);
+    console.log(yourMessage);
+  }
+  const handlefullName = (e) => {
+    setfullName(e.target.value);
+  };
+  const changePhoneNumber = (e) => {
+    console.log(e);
+  };
+
+  const setvalidateEmail = (e) => {
+    e.preventDefault();
+    const email = e.target.value;
+    
+    if (validator.isEmail(email)) {
+      setEmail(email);
+      setEmailError("");
+    } else {
+      setEmailError("Enter valid Email!");
+    }
+  };
   return (
     <div className="contact">
       <div className="row pa-10 mt-10">
@@ -16,35 +52,67 @@ export default function Contact() {
         </p>
       </div>
       <div className="row d-block mt-5">
-        <div className="col-10 mx-auto">
+        <form onSubmit={handleSubmit} className="col-10 mx-auto">
           <div className="row m-2">
-            <label className="col-6 me-5">
-              <span className="col-8 me-5">Full Name:</span>
+            <label className="col-5 d-flex">
+              <span className="me-5">Full Name:</span>
               <input
-                className="col-6 ml-6"
+                className="col-7 ml-6"
                 type="input"
                 name="fullName"
+                value={fullName}
+                onChange={handlefullName}
                 required
               />
             </label>
-            <label className="col-5">
-                <PhoneInput
-                  country={"us"}
-                  containerClass="col-4"
-                  specialLabel="Custom Phone*"
-                ></PhoneInput>
-            </label>
+            <div className="col-6 d-flex">
+              <label className="">Ph No:</label>
+              <PhoneInput
+                country={"us"}
+                value={phoneNum}
+                onChange={(e) => setPhoneNum(e)}
+                containerClass="col-4"
+              ></PhoneInput>
+            </div>
           </div>
           <div className="row m-2">
             <span className="col-2">Your email:</span>
-            <input className="col-8" type="email" name="email" required />
+            <input
+              className="col-8"
+              type="text"
+              name="email"
+              onChange={(e) => setvalidateEmail(e)}
+              required
+            />
+            <span
+              style={{
+                fontWeight: "bold",
+                color: "red",
+              }}
+            >
+              {emailError}
+            </span>
           </div>
           <div className="row m-2">
             <span className="col-2">Your message:</span>
-            <textarea className="col-8" name="message" required></textarea>
+            <textarea
+              className="col-8"
+              name="message"
+              value={yourMessage}
+              onChange={(e) => setYourMessage(e.target.value)}
+              required
+            ></textarea>
           </div>
-          <button className="col-3 mt-4 btn btn-primary">Submit</button>
-        </div>
+          <div className="row mt-5">
+            <div className="col-8"></div>
+            <button
+              className="col-2 me-4 btn btn-primary"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
