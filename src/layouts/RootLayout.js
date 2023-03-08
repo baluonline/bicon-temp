048cc9e14a58a188767cf169fb03d019edc4e06d
baluonline <18106894+baluonline@users.noreply.github.com>
2023-03-07 19:24:54 -0600

    routes updated

--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -2,7 +2,7 @@ import { Outlet, NavLink, Link, useNavigate   } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import logo from "..//images/logo.png";
+import logo from "../images/logo.png";
 import "../styles/footer.scss";
 import Footer from "./Footer";
 import Button from 'react-bootstrap/Button';
@@ -10,30 +10,30 @@ import Button from 'react-bootstrap/Button';
 export default function RootLayout() {
   const navigate = useNavigate();
   const clickContactUs = () => {
-    navigate("/bicon-temp/contact")
+    navigate("/contact")
   }
   return (
     <div className="root-layout">
       <header className="col-12 d-flex">
         <Navbar className="col-12 " bg="light" expand="lg">
           <Container>
-            <img href="/bicon-temp" className="App-logo" src={logo} />
-            <Navbar.Brand as={Link} to="/bicon-temp">
+            <img href="/" className="App-logo" src={logo} />
+            <Navbar.Brand as={Link} to="/">
               Bicon
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link as={Link} to="/bicon-temp/">
+                <Nav.Link as={Link} to="/">
                   Home
                 </Nav.Link>
-                <Nav.Link as={Link} to="/bicon-temp/technology">
+                <Nav.Link as={Link} to="/technology">
                   Techonology
                 </Nav.Link>
-                <Nav.Link as={Link} to="/bicon-temp/consulting">
+                <Nav.Link as={Link} to="/consulting">
                   Consulting
                 </Nav.Link>
-                <Nav.Link as={Link} to="/bicon-temp/about">
+                <Nav.Link as={Link} to="/about">
                   About Us
                 </Nav.Link>
               </Nav>
